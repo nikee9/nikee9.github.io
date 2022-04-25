b1a615672949209449ba5644d05a927eefc02dfe
Nikhil Gandhi <ngnikee67@hotmail.com>
2022-04-25 15:18:26 +0100

    docs for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-//import preprocess from 'svelte-preprocess';
+import adapter  from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	//preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+            pages: "docs",
+            assets: "docs"
+        }),
 	}
 };
 
